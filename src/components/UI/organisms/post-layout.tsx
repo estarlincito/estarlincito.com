a2516442fde90ef0ecded74e480fe249d203110a
Estarlin R <89669432+estarlincito@users.noreply.github.com>
2023-04-12 00:34:59 -0400

    updated posts-layout

--- src/components/UI/organisms/post-layout.tsx
+++ src/components/UI/organisms/post-layout.tsx
@@ -6,6 +6,83 @@ import { TbArrowBadgeRight, TbBrandGoogleHome } from 'react-icons/tb';
 import PostMdx from './post-mdx';
 import Wrapper from './wrapper';
 
+interface LiProps {
+  children: React.ReactNode;
+  href: string;
+  className?: string;
+}
+const Li: React.FC<LiProps> = ({ children, href, className }) => {
+  return (
+    <li className={className}>
+      <_Link href={href} target='_self' className='flex items-center'>
+        {children}
+      </_Link>
+    </li>
+  );
+};
+
+interface NavProps {
+  category: string;
+  subcategory: string;
+  slug_category: string;
+  slug_subcategory: string;
+}
+
+const Nav: React.FC<NavProps> = ({
+  category,
+  subcategory,
+  slug_category,
+  slug_subcategory,
+}) => {
+  return (
+    <ul
+      className={clsx(
+        'flex items-center gap-x-3 mb-3 select-none',
+        'text-black/70 dark:text-white/70'
+      )}
+    >
+      <Li href='/'>
+        <TbBrandGoogleHome />
+      </Li>
+
+      <Li href={slug_category}>
+        <TbArrowBadgeRight />
+        <span>{category}</span>
+      </Li>
+
+      <Li href={slug_subcategory}>
+        <TbArrowBadgeRight />
+        <span>{subcategory}</span>
+      </Li>
+    </ul>
+  );
+};
+
+interface TagsProps {
+  tags: string[];
+}
+const Tags: React.FC<TagsProps> = ({ tags }) => {
+  return (
+    <div className='mt-12 text-sm w-80'>
+      <h5 className='font-bold'>Tags:</h5>
+      <ul className='flex'>
+        {tags.map((tag, id) => (
+          <li key={id} className='m-1'>
+            <_Link
+              href={`/blog/tags/${tag}`}
+              target='_self'
+              font='font-normal'
+              className={clsx('text-sky-600', 'dark:text-sky-400')}
+            >
+              #{tag}
+            </_Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 const PostLayout: React.FC<Blog> = ({
   teme,
   category,
@@ -19,39 +96,13 @@ const PostLayout: React.FC<Blog> = ({
   return (
     <Wrapper>
       <section>
-        <div className='flex flex-row gap-5 select-none mb-3'>
-          <_Link href='/' target='_self' className='flex items-center'>
-            <TbBrandGoogleHome />
-          </_Link>
-
-          <_Link
-            href={slug_category}
-            target='_self'
-            className={clsx(
-              'text-sky-600',
-              'dark:text-sky-400',
-              'flex items-center'
-            )}
-          >
-            <TbArrowBadgeRight />
-            <span className='pl-2'>{category}</span>
-          </_Link>
-
-          <_Link
-            href={slug_subcategory}
-            target='_self'
-            className={clsx(
-              'text-sky-600',
-              'dark:text-sky-400',
-              'flex items-center'
-            )}
-          >
-            <TbArrowBadgeRight />
-            <span>{subcategory}</span>
-          </_Link>
-        </div>
-
-        <Title className='text-3xl text-center mb-10'>{teme}</Title>
+        <Nav
+          category={category}
+          subcategory={subcategory}
+          slug_category={slug_category}
+          slug_subcategory={slug_subcategory}
+        />
+        <Title className='text-2xl mb-3'>{teme}</Title>
 
         <PostMdx doc={body.code} />
 
@@ -59,24 +110,8 @@ const PostLayout: React.FC<Blog> = ({
           <b>Última actualización:</b> el&nbsp;
           <time dateTime={publishedAt}>{dateFormat(publishedAt)}</time>
         </Paragraph>
-      </section>
 
-      <section className='mt-12 text-sm w-80'>
-        <h5 className='font-bold'>Tags:</h5>
-        <ul className='flex'>
-          {tags.map((tag, id) => (
-            <li key={id} className='m-1'>
-              <_Link
-                href={`/blog/tags/${tag}`}
-                target='_self'
-                font='font-normal'
-                className={clsx('text-sky-600', 'dark:text-sky-400')}
-              >
-                #{tag}
-              </_Link>
-            </li>
-          ))}
-        </ul>
+        <Tags tags={tags} />
       </section>
     </Wrapper>
   );
