515459593720928584e6e728c27f51b1dc2ca754
Estarlin R <estarlincito@Estarlincito-2.local>
2023-05-03 21:42:17 -0400

    change a target condition

--- src/components/UI/organisms/postlayout/post/a.tsx
+++ src/components/UI/organisms/postlayout/post/a.tsx
@@ -1,8 +1,9 @@
 import { Linkdoc } from '@/components/HOC';
 
 const A: React.FC<JSX.IntrinsicElements['a']> = ({ children, href }) => {
-  const target = href!.includes('https://');
+  const target = href!.includes('https://estarlincito.com');
 
+  //https only mode
   if (href!.includes('http://')) {
     throw new Error("You have 'http://' please change");
   }
@@ -10,7 +11,7 @@ const A: React.FC<JSX.IntrinsicElements['a']> = ({ children, href }) => {
   return (
     <Linkdoc
       href={href !== undefined ? href : '/no-found'}
-      target={target == true ? '_blank' : '_self'}
+      target={target == true ? '_self' : '_blank'}
       underline={true}
       color='sky'
     >
