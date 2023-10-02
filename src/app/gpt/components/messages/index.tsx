74fdb0a59a611e346d0bffe1e29a5169c28d324a
Estarlin R <estarlincito@Estarlincitos-Macbook-Air.local>
2023-10-02 12:18:33 -0400

    disable smooth

--- src/app/gpt/components/messages/index.tsx
+++ src/app/gpt/components/messages/index.tsx
@@ -1,24 +1,23 @@
 'use client';
 import { Flex, ScrollArea } from '@radix-ui/themes';
 import { Message } from 'ai';
-import { useEffect, useRef } from 'react';
 import Content from './content';
 
 interface MessagesPros {
   messages: Message[];
 }
 const Messages = ({ messages }: MessagesPros) => {
-  const smooth = useRef<HTMLDivElement>(null);
+  // const smooth = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    smooth.current?.scrollTo({
-      top: smooth.current?.scrollHeight,
-      behavior: 'smooth',
-    });
-  }, [messages]);
+  // useEffect(() => {
+  //   smooth.current?.scrollTo({
+  //     top: smooth.current?.scrollHeight,
+  //     behavior: 'smooth',
+  //   });
+  // }, [messages]);
 
   return (
-    <ScrollArea type='hover' scrollbars='vertical' ref={smooth}>
+    <ScrollArea type='hover' scrollbars='vertical'>
       <Flex p='8' gap='5' direction='column'>
         {messages.map(({ role, content, id }) => (
           <Flex key={id} direction='column' gap='3'>
