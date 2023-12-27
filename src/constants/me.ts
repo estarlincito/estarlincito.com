f12ca6410777d1dda822caacd9d356afd64bc959
Estarlin R <estarlincito@Estarlincito-Air.local>
2023-12-27 14:47:43 -0400

    ʕ•ᴥ•ʔ me

--- src/constants/me.ts
+++ src/constants/me.ts
@@ -17,9 +17,49 @@ class Seo {
 
 const me = [
   {
+    principio: 'Decisiones',
+    razon: '¿Afecta X tu amor propio?',
+    id: 1,
+  },
+  {
+    principio: '10 minutos al día',
+    razon:
+      '¿Qué tal si regalas 10 minutos solo para ti, para encontrar paz y reflexionar en meditación? (;',
+    id: 2,
+  },
+  {
+    principio: 'Ruido mental',
+    razon: '¿Estás realmente presente en este momento?',
+    id: 3,
+  },
+  {
     principio: 'No ser el amante de nadie',
     razon: 'Porque soy una persona valiosa y merezco mucho más que migajas.',
-    id: 1,
+    id: 4,
+  },
+  {
+    principio: 'No mendigues la atención de nadie y mucho menos amor',
+    razon:
+      'Al final es simple, la persona que te merece es aquella que teniendo la libertad de elegir, se acerca a ti, te aprecia y te dedica tiempo y pensamientos.',
+    id: 5,
+  },
+  {
+    principio: 'No corras detrás de alguien que ya sabe dónde estás',
+    razon:
+      'Así que uno planta su propio jardín y decora su propia alma, en lugar de esperar a que alguien le traiga flores',
+    id: 6,
+  },
+  {
+    principio: 'Si tus planes no gustan, vete solo: alguien te encontrará',
+    razon:
+      'Si tus planes no gustan, si tus sueños no encajan en moldes ajenos, hazlo: vete solo',
+    id: 7,
+  },
+  {
+    principio: 'El carácter exepcional de las buenas personas',
+    razon:
+      'Por eso, más que intentar ser diferente, intenta ser bueno, puesto que siendo bueno serás diferente.',
+    id: 8,
   },
 ];
 
