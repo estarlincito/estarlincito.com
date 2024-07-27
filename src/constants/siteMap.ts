bc03f8ed7d0cc3c179fe56a5faa0be482c078228
Estarlin R <estarlincito@Estarlincito-Air.local>
2024-07-27 12:06:43 -0400

    removed ikigay and me, also add books

--- src/constants/siteMap.ts
+++ src/constants/siteMap.ts
@@ -3,9 +3,6 @@ const sitemap = [
   { label: 'Blog', route: '/blog' },
   { label: 'Tags', route: '/blog/tags' },
   { label: 'GPT', route: '/gpt' },
-  { label: 'ʕ•ᴥ•ʔ me', route: '/me' },
-  // { label: 'Metas', route: '/metas' },
-  // { label: 'Ikigai', route: '/ikigai' },
   // { label: 'Acerca de', route: '/about' },
   { label: 'Contacto', route: '/contact' },
 ];
